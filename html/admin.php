4bc43cfbc31ee0509d6cde0cdb257e5faa7581db
Sungket <sungket@github.com>
2025-02-11 14:49:51 +0000

    Added the admin.php file

---
+++ html/admin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Page</title>
+        <link rel="stylesheet" href="css/style.css"/>
+    </head>
+    <h1>Hello I am from the php file!</h1>
+    <body>
+        <form method="post" action="admin.php">
+
+    
+
+        </form>
+    </body>
+
+</html>
+
+<?php
+
+?>
