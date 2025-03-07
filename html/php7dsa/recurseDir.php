b887a19e851ec3cfb811fd5c70a86bac08288cbd
Sungket <sungket_rhymes@hotmail.com>
2025-03-07 15:17:28 +0000

    added tree.php

---
+++ html/php7dsa/recurseDir.php
@@ -0,0 +1,25 @@
+<?php
+
+function showFiles(string $dirName, Array &$allFiles = []) {
+    $files = scandir($dirName);
+
+    foreach ($files as $key => $value) {
+        $path = realpath($dirName . DIRECTORY_SEPARATOR . $value);
+        if (!is_dir($path)) {
+            $allFiles[] = $path;
+        } else if ($value != "." && $value != "..") { 
+            showFiles($path, $allFiles);
+            $allFiles[] = $path;
+        }
+    }
+    return;
+}
+
+$files = [];
+
+showFiles(".", $files);
+
+foreach($files as $file) {
+    echo $file."<br>";
+}
+
---
+++ html/php7dsa/tree.php
@@ -0,0 +1,6 @@
+<?php
+
+class TreeNode {
+    public $data = NULL;
+    
+}
