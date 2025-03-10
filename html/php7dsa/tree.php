93e40b92396b2a6c1d00840fbe861fc304207647
Sungket <sungket_rhymes@hotmail.com>
2025-03-10 09:19:14 +0000

    completed tree.php

--- html/php7dsa/tree.php
+++ html/php7dsa/tree.php
@@ -2,5 +2,34 @@
 
 class TreeNode {
     public $data = NULL;
-    
+    public $children = [];
+
+    public function __construct(string $data = NULL)
+    {
+        $this->data = $data;
+    }
+
+    public function addChildren(TreeNode $node) 
+    {
+        $this->children[] = $node;
+    }
+}
+
+class Tree {
+    public $root = NULL;
+    public function __construct(TreeNode $node)
+    {
+        $this->root = $node;
+    }
+
+    public function traverse (TreeNode $node, int $level = 0) {
+        if ($node) {
+            echo str_repeat("-", $level);
+            echo $node->data . "<br>";
+
+            foreach ($node->children as $childNode) {
+                $this->traverse($childNode, $level + 1);
+            }
+        }
+    }
 }
