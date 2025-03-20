94f2ec2e8f3e8cbe6e1466f99e24408eb39cd881
Sungket <sungket_rhymes@hotmail.com>
2025-03-20 13:38:56 +0000

    added fibonacci.php

---
+++ html/php7dsa/fibonacci.php
@@ -0,0 +1,42 @@
+<?php
+
+$startTime = microtime();
+$fibCache = [];
+$count = 0;
+
+function fibonacciMemoized(int $n) : int {
+    global $fibCache;
+    global $count;
+    $count++;
+    // if ($n == 0) {
+    //     return 1;
+    // } else if ($n == 1) {
+    //     return 1;
+    // } else {
+    //     return fibonacci($n-1) + fibonacci($n -2);
+    // }
+    if ($n == 0 || $n == 1) {
+        return 1;
+    } else {
+        if (isset($fibCache[$n - 1])) {
+            $tmp = $fibCache[$n - 1];
+        } else {
+            $tmp = fibonacciMemoized($n-1);
+            $fibCache[$n - 1] = $tmp;
+        }
+
+        if (isset($fibCache[$n-2])) {
+            $tmp1 = $fibCache[$n - 2];
+        } else {
+            $tmp1 = fibonacciMemoized($n - 2);
+            $fibCache[$n - 2] = $tmp1;
+        }
+        return $tmp + $tmp1;
+    }
+}
+
+echo fibonacciMemoized(30) . "<br>";
+echo "Function called: " . $count . "<br>";
+$endTime = microtime();
+echo "time =" . ($endTime - $startTime) . "<br>";
+
