41fc3137831ca9651cb68df9efcd3c2885121cb6
Sungket <sungket_rhymes@hotmail.com>
2025-03-23 12:38:50 +0000

    Added pearson file

---
+++ html/php7dsa/pearson.php
@@ -0,0 +1,17 @@
+<?php
+
+$reviews['Adiyan'] = ["McDonalds" => 5, "KFC" => 5, "Pizza Hut" => 4.5, "Burger King" => 4.7,
+    "American Burger" => 3.5, "Pizza Roma" => 2.5];
+$reviews['Mikhael'] = ["McDonalds" => 3, "KFC" => 4, "Pizza Hut" => 3.5, "Burger King" => 4,
+    "American Burger" => 4, "Jafran" => 4];
+$reviews['Zayeed'] = ["McDonalds" => 5, "KFC" => 4, "Pizza Hut" => 2.5, "Burger King" => 4.5,
+    "American Burger" => 3.5, "Sbarro" => 2];
+$reviews['Arush'] = ["KFC" => 4.5, "Pizza Hut" => 3, "Burger King" => 4, "American Burger" => 3,
+    "Jafran" => 2.5, "FFC" => 3.5];
+$reviews['Tajwar'] = ["Burger King" => 3, "American Burger" => 2, "KFC" => 2.5, "Pizza Hut" => 3,
+    "Pizza Roma" => 2.5, "FFC" => 3];
+$reviews['Aayan'] = ["KFC" => 5, "Pizza Hut" => 4, "Pizza Roma" => 4.5, "FFC" => 4];
+
+function pearsonScore(array $reviews, string $person1, string $person2):float {
+    
+}
