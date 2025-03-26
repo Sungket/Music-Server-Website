d7d78aec1f83b400bc25c7e4c4be709c65977689
Sungket <sungket_rhymes@hotmail.com>
2025-03-26 15:53:18 +0000

    deleted some redundant classes

--- html/php/config.php
+++ html/php/config.php
@@ -13,4 +13,18 @@
 	$cd_password = "saturn";
 
 
+	class Dbh {
+		private $host = "127.0.0.1";
+		private $user = "sungket";
+		private $pwd = "saturn";
+		private $dbname = "rajAdmin";
+
+		protected function connect() {
+			$dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+			$pdo = new PDO($dsn, $this->user, $this->pwd);
+			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			return $pdo;
+		}
+	}
+
 ?>
