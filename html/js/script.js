70eeb323703be9649224a3a8306491cd4bc8a0e4
Sungket <sungket_rhymes@hotmail.com>
2025-05-08 16:06:35 +0100

    added playBtn variable to script.js and also modified index.php with onclick function on play button

--- html/js/script.js
+++ html/js/script.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
     // populate the front-end by reading in the uploads directory
-    $.ajax({
-        url: "../php/scanDirectory.php",
-        type: "GET",
-        success: function(result){
-            alert(result);
-        }
-    })
+
 })
+
+let playBtn = document.querySelector("");
