a6af4453d6b4698eeefd7c0b18fd17c4d30753e7
Sungket <sungket_rhymes@hotmail.com>
2025-05-14 14:11:01 +0100

    cleared up comments in script.js

--- html/js/script.js
+++ html/js/script.js
@@ -1,15 +1,5 @@
-//let playTrack = document.getElementById("playTrack");
-//let curr_track = document.createElement("audio");
-//curr_track.setAttribute("src", "uploads/Hello_World.mp3");
-//curr_track.src = "../uploads/Hello_World.mp3";
-//curr_track.load();
-
 function playTrack() {
     //play the loaded track
     const curr_track = new Audio("../uploads/Hello_World.mp3");
     curr_track.play();
 }
-
-
-
-//playTrack.addEventListener("click", playTrack);
